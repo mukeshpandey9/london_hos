98c7d2fd5f6d2657acf5d5781e7e3ac9058078ba
Mukesh Pandey <panditmukki5@gmail.com>
2024-07-05 19:06:06 +0530

    To Top

--- frontend/src/components/Layout/ScrollToTop.jsx
+++ frontend/src/components/Layout/ScrollToTop.jsx
@@ -1,12 +1,15 @@
 // ScrollToTop.js
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   }, [pathname]);
 
   return null;
